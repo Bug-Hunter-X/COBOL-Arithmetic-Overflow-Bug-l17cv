1c113bc0110cfb0006ee9baacacca3984e1b4b25
BugHunter-X <askzenai@gmail.com>
2024-12-08 08:20:07 +0100

    Add bug.cob file

---
+++ bug.cob
@@ -0,0 +1,12 @@
+01  WS-AREA-1.                                         
+     05  WS-VAR-1 PIC 9(5) VALUE 0. 
+     05  WS-VAR-2 PIC 9(5) VALUE 0. 
+01  WS-AREA-2.                                         
+     05  WS-VAR-3 PIC 9(5) VALUE 0. 
+     05  WS-VAR-4 PIC 9(5) VALUE 0. 
+
+PROCEDURE DIVISION. 
+    ADD WS-VAR-1 WS-VAR-2 GIVING WS-VAR-3.  
+    ADD WS-VAR-3 WS-VAR-4 GIVING WS-VAR-3. 
+    DISPLAY "WS-VAR-3:" WS-VAR-3. 
+    STOP RUN.
